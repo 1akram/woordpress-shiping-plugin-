1b6521299298644f086dacf492e8efe39d31495a
Sami-21 <samovitch360@gmail.com>
2024-12-10 21:50:05 +0100

    initial setup

---
+++ admin/transport-company-plugin-admin.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/admin
+ */
+
+use Carbon_Fields\Container\Container;
+use Carbon_Fields\Field\Field;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/admin
+ * @author     Your Name <email@example.com>
+ */
+class Transportation_Company_Admin
+{
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct($plugin_name, $version)
+	{
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+		add_action("admin_menu", array($this, "addAdminMenuItems"));
+		add_action('after_setup_theme', function () {
+			Carbon_Fields::boot();
+		});
+	}
+
+
+
+	/**
+	 * @return [type]
+	 */
+	public function addAdminMenuItems()
+	{
+		add_menu_page(
+			'Transportation',      // Page title
+			'Transportation',           // Menu title
+			'manage_options',      // Capability required to access this menu
+			'transportation-companies',           // Slug of the menu
+			array($this, 'my_plugin_main_page'), // Callback function to render the page
+			'dashicons-car', // Icon URL or Dashicon class
+			6                      // Position in the menu order
+		);
+
+		// Add a submenu
+		add_submenu_page(
+			'transportation-companies',           // Parent slug
+			'New company',            // Page title
+			'New company',            // Menu title
+			'manage_options',      // Capability required to access this submenu
+			'my-plugin-settings',  // Slug of the submenu
+			array($this, 'my_plugin_settings_page'), // Callback function to render the submenu page
+			2
+		);
+	}
+
+	function my_plugin_main_page()
+	{
+		Container::make('theme_options', __('Theme Options'))
+			->add_fields(array(
+				Field::make('text', 'crb_facebook_url', __('Facebook URL')),
+				Field::make('textarea', 'crb_footer_text', __('Footer Text'))
+			));
+	}
+
+	function my_plugin_settings_page()
+	{
+		Container::make('theme_options', __('Theme Options'))
+			->add_fields(array(
+				Field::make('text', 'crb_facebook_url', __('Facebook URL')),
+				Field::make('textarea', 'crb_footer_text', __('Footer Text'))
+			));
+	}
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles()
+	{
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/plugin-name-admin.css', array(), $this->version, 'all');
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts()
+	{
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/plugin-name-admin.js', array('jquery'), $this->version, false);
+	}
+}
